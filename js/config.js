f392d8123b06f5c51d6f5e277b2b402bf95e9553
Maarten van Bree <63168360+mvb-nerdzone@users.noreply.github.com>
2023-07-18 14:38:25 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ let respecConfig = {
   useLogo: true,
   useLabel: true,
   license: "cc-by",
-  specStatus: "WV",
+  specStatus: "DEF",
   specType: "HR",
   pubDomain: "ep",
   shortName: "ReSpec ep test",
